2a92af3b809db81687cd7b652fe405c15d26ef13
David Macedo <newlima159@gmail.com>
2024-06-12 13:22:17 -0300

    feat: client-card component

---
+++ src/app/components/client-card/client-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-card',
+  templateUrl: './client-card.component.html',
+  styleUrl: './client-card.component.scss'
+})
+export class ClientCardComponent {
+
+  ClientIcon: string = "../../../assets/icons/icon-client.png"
+}
