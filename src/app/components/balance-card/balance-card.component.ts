b7c00333aa14499bf6518b6ec79b17000bf4c2c2
David Macedo <newlima159@gmail.com>
2024-06-12 13:22:45 -0300

    feat: balance-card component

---
+++ src/app/components/balance-card/balance-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balance-card',
+  templateUrl: './balance-card.component.html',
+  styleUrl: './balance-card.component.scss'
+})
+export class BalanceCardComponent {
+
+  handIcon: string = "../../../assets/icons/icon-hand.png" ;
+}
